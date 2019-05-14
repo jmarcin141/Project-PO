d695336079ef567e2c933bc980673810cbd6b92b
Jakub Marcinkowski <jmarcin@student.agh.edu.pl>
2019-05-14 19:33:39 +0200

    Konfigiracja gitHuba, obsluga interfejsu

---
+++ Source.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+
+int main(void)
+{
+	system("Color 0B");
+	std::cout << "Efekt gitarowy - projekt" << std::endl;
+
+	std::cout << "Test Git/n";
+
+
+	system("Pause");
+	return 0;
+}
